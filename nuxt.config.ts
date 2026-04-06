f234dfcb4a31ca9a2cc295100ce344e9d9923ce0
Kun vinthien <kunvinthien@gmail.com>
2026-04-06 17:35:53 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    nitro: {
+    preset: 'cloudflare-pages'   // ← add this
+  },
   ssr: true,
   devtools: { enabled: true },
   buildDir: process.env.NODE_ENV === 'production' ? '.nuxt-build' : '.nuxt',
